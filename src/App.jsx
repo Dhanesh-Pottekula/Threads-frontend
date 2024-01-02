4d9f7d24988c18d524073403c3626ae834c4ca65
dhanesh <dhaneshpk359@gmail.com>
2024-01-02 23:15:06 +0530

    fir

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { Button, Container } from "@chakra-ui/react";
 import "./App.css";
-import { Route,Routes} from "react-router-dom";
+import { Navigate, Route,Routes} from "react-router-dom";
 import Userpage from "./pages/Userpage";
 import PostPage from "./pages/PostPage";
 import Header from "./components/Header";
 import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import { useRecoilValue } from "recoil";
+import userAtom from "./atoms/userAtom";
 
 function App() {
+  const user = useRecoilValue(userAtom)
+  console.log(user)
   return (
     <>
       <Container maxW="620px">
         <Header />
           <Routes>
+            <Route path="/" element={user?<HomePage/> : <Navigate to={'/auth'}/>} />
+            <Route path="/auth" element={!user?<AuthPage/> : <Navigate to={'/'}/>} />
             <Route path="/:username" element={<Userpage />} />
-            <Route path="/auth" element={<AuthPage />} />
             <Route path="/:username/post/:pid" element={<PostPage />} />
           </Routes>
        
